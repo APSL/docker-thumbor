75f53e246e0b244846f3f5344d2bdf96ce0ad848
Hussain Parsaiyan <hus787@gmail.com>
2016-01-07 17:08:38 +0200

    Update docker-entrypoint.sh

--- thumbor/docker-entrypoint.sh
+++ thumbor/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 envtpl /usr/src/app/thumbor.conf.tpl  --allow-missing --keep-template
 
 if [ "$1" = 'thumbor' ]; then
-    exec thumbor --port=80 --conf=/usr/src/app/thumbor.conf
+    exec thumbor --port=28765 --conf=/usr/src/app/thumbor.conf
 fi
 
 exec "$@"
