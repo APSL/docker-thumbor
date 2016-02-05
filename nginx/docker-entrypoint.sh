399c4fab0d76885cb58f74898564a1624087aae8
Edu Herraiz Aparicio <gshark@gmail.com>
2016-02-05 17:57:25 +0100

    Envars fix and logs nginx tested

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -2,22 +2,22 @@
 set -e
 
 # If not set we use thumbor host default value
-if [ -z ${THUMBOR_HOST+x} ]; then
-    THUMBOR_HOST=thumbor
+if [ -z $THUMBOR_DEFAULT_HOST ]; then
+    THUMBOR_DEFAULT_HOST=thumbor
 fi
 
 # If not set we use thumbor port default value
-if [ -z ${THUMBOR_PORT+x} ]; then
-    THUMBOR_PORT=8000
+if [ -z $THUMBOR_DEFAULT_PORT ]; then
+    THUMBOR_DEFAULT_PORT=8000
 fi
 
 # If not set we use 80 for nginx port
-if [ -z ${NGINX_PORT+x} ]; then
-    NGINX_PORT=80
+if [ -z $NGINX_DEFAULT_PORT ]; then
+    NGINX_DEFAULT_PORT=80
 fi
 
-sed -i -e "s/server thumbor_host:thumbor_port/server $THUMBOR_HOST:$THUMBOR_PORT/g"  /etc/nginx/nginx.conf
-sed -i -e "s/listen 80 default/listen $NGINX_PORT default/g"  /etc/nginx/nginx.conf
+sed -i -e "s/server thumbor_host:thumbor_port/server $THUMBOR_DEFAULT_HOST:$THUMBOR_DEFAULT_PORT/g"  /etc/nginx/nginx.conf
+sed -i -e "s/listen 80 default/listen $NGINX_DEFAULT_PORT default/g"  /etc/nginx/nginx.conf
 
 if [ "$1" = 'nginx-daemon' ]; then
     exec nginx -g "daemon off;";
